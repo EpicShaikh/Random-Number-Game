73e4ba255e7c83dc9925ea37fca3f55b2604291a
abdurraheemsha1 <29732474-abdurraheemsha1@users.noreply.replit.com>
2024-02-12 17:50:27 +0000

    got the main bit of the home page done.

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import os
 import random
 import time
 from flask import Flask
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def home():
+  page = ""
+  f = open("pages/home.html", "r")
+  page = f.read()
+  f.close()
+  return page
+  
+
+
+app.run(host='0.0.0.0', port=81)
